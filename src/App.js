75fd4cf7144a07d05d7754d5a0e7bfbbc21bcb5c
bsdaoquang <bsdaoquang@gmail.com>
2023-10-11 21:37:18 +0700

    edit app file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return <div className='App'>Hello World!!! npm start yarn start</div>;
+	return <div className='App'>Hello World!!!</div>;
 }
 
 export default App;
