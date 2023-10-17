bc792d4515a04a6bf7e9fc1e481cdd4f6204425f
bsdaoquang <bsdaoquang@gmail.com>
2023-10-17 21:46:20 +0700

    state

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,51 @@
 /** @format */
 
-import React from 'react';
-import UserComponent from '../components/UserComponent';
-import HeaderComponent from '../components/HeaderComponent';
-import { users } from '../data/users';
-import CardComponent from '../components/CardComponent';
+import React, { useState } from 'react';
 
 const HomeScreen = () => {
+	const [likeState, setLikeState] = useState('');
+
+	const [username, setUsername] = useState('');
+
+	const color = 'red';
+	const name = 'MindX';
+
 	return (
 		<div>
-			<HeaderComponent />
+			<p>{username}</p>
+			<input
+				type='text'
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+			/>
+			<h2 style={{ color }}>Title: {name}</h2>
+			<h1
+				style={{
+					color,
+				}}>
+				{likeState}
+			</h1>
+			<br />
 
-			<CardComponent color='coral'>
-				<p>Profile screen</p>
-			</CardComponent>
-
-			<CardComponent>
-				{users.map((user) => (
-					<UserComponent
-						key={user.id}
-						name={user.name}
-						email={user.email}
-						company={user.company}
-					/>
-				))}
-			</CardComponent>
+			<button onClick={() => alert(username)}>Show username</button>
+			<button onClick={() => setUsername('')}>clear</button>
+			<div
+				style={{
+					padding: 10,
+				}}>
+				<button
+					onClick={() => {
+						setLikeState('Liked');
+					}}>
+					Like
+				</button>
+				<button
+					onClick={() => {
+						setLikeState('DisLike');
+					}}>
+					Dislike
+				</button>
+			</div>
 		</div>
 	);
 };
