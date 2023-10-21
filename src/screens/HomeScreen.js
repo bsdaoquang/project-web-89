4fb5fe95af264d2b47d9607f5639cd49eb4b726f
bsdaoquang <bsdaoquang@gmail.com>
2023-10-21 20:38:28 +0700

    props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,49 +3,11 @@
 import React, { useState } from 'react';
 
 const HomeScreen = () => {
-	const [likeState, setLikeState] = useState('');
-
-	const [username, setUsername] = useState('');
-
-	const color = 'red';
-	const name = 'MindX';
+	const [count, setCount] = useState(0);
 
 	return (
 		<div>
-			<p>{username}</p>
-			<input
-				type='text'
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-			/>
-			<h2 style={{ color }}>Title: {name}</h2>
-			<h1
-				style={{
-					color,
-				}}>
-				{likeState}
-			</h1>
-			<br />
-
-			<button onClick={() => alert(username)}>Show username</button>
-			<button onClick={() => setUsername('')}>clear</button>
-			<div
-				style={{
-					padding: 10,
-				}}>
-				<button
-					onClick={() => {
-						setLikeState('Liked');
-					}}>
-					Like
-				</button>
-				<button
-					onClick={() => {
-						setLikeState('DisLike');
-					}}>
-					Dislike
-				</button>
-			</div>
+			<h1>asfsfsafas</h1>
 		</div>
 	);
 };
