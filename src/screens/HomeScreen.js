07ba916221bc1eda6c0c879b302d3b53a30c6f94
bsdaoquang <bsdaoquang@gmail.com>
2023-10-21 21:32:58 +0700

    custom form

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,11 +3,63 @@
 import React, { useState } from 'react';
 
 const HomeScreen = () => {
-	const [count, setCount] = useState(0);
+	const [profile, setProfile] = useState({
+		name: '',
+		email: '',
+		address: '',
+		phone: '',
+		gender: 'male',
+	});
+
+	const handleSetProfile = (key, val) => {
+		const items = profile;
+
+		items[`${key}`] = val;
+
+		setProfile({ ...items });
+
+		// Biến tham chiếu -> lưu địa chỉ ô nhớ, nên muốn thay đổi thì phải thay đổi địa chỉ ô nhớ
+		// biến tham trị -> lưu giá trị của ô nhớ, nên muốn thay đổi thì chỉ cần thay đổi giá trị ô nhớ
+	};
 
 	return (
 		<div>
-			<h1>asfsfsafas</h1>
+			<input
+				type='text'
+				value={profile.name2}
+				name=''
+				onChange={(val) => handleSetProfile('name2', val.target.value)}
+				id=''
+			/>
+			<input
+				type='text'
+				value={profile.address}
+				name=''
+				onChange={(val) => handleSetProfile('address', val.target.value)}
+				id=''
+			/>
+			<input
+				type='text'
+				value={profile.email}
+				name=''
+				onChange={(val) => handleSetProfile('email', val.target.value)}
+				id=''
+			/>
+			<input
+				type='text'
+				value={profile.phone}
+				name=''
+				onChange={(val) => handleSetProfile('phone', val.target.value)}
+				id=''
+			/>
+			<input
+				type='text'
+				value={profile.gender}
+				name=''
+				onChange={(val) => handleSetProfile('gender', val.target.value)}
+				id=''
+			/>
+			<button onClick={() => console.log(profile)}>get input</button>
 		</div>
 	);
 };
