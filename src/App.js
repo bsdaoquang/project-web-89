22c54636fdc211148f034050b1456d3e746418cc
bsdaoquang <bsdaoquang@gmail.com>
2023-10-28 21:32:23 +0700

    add go to top button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import { Avatar, Button, List, Space } from 'antd';
+import { Avatar, Button, FloatButton, List, Space } from 'antd';
 import { Edit } from 'iconsax-react';
 import { useEffect, useState } from 'react';
 import './App.css';
@@ -9,6 +9,7 @@ import UserComponent from './components/UserComponent';
 
 function App() {
 	const [data, setData] = useState([]);
+	const [height, setHeight] = useState(0);
 
 	useEffect(() => {
 		getPosts();
@@ -19,33 +20,36 @@ function App() {
 	};
 
 	return (
-		<div className='container mt-4'>
-			<div className='row'>
-				<div className='col-8 offset-2'>
-					<List
-						dataSource={data}
-						renderItem={(item, _index) => (
-							<List.Item
-								key={item.id}
-								extra={
-									<Space>
-										<Button
-											onClick={() => console.log(item)}
-											type='text'
-											icon={<Edit size={18} color='coral' />}
-										/>
-									</Space>
-								}>
-								<List.Item.Meta
-									title={item.title}
-									description={<UserComponent id={item.userId} />}
-								/>
-							</List.Item>
-						)}
-					/>
+		<>
+			<div className='container mt-4'>
+				<div className='row'>
+					<div className='col-8 offset-2'>
+						<List
+							dataSource={data}
+							renderItem={(item, _index) => (
+								<List.Item
+									key={item.id}
+									extra={
+										<Space>
+											<Button
+												onClick={() => console.log(item)}
+												type='text'
+												icon={<Edit size={18} color='coral' />}
+											/>
+										</Space>
+									}>
+									<List.Item.Meta
+										title={item.title}
+										description={<UserComponent id={item.userId} />}
+									/>
+								</List.Item>
+							)}
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
+			<FloatButton.BackTop />
+		</>
 	);
 }
 
